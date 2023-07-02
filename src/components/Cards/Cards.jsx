f22b3a12a828cb726a0a6a038b80b90daef55a3e
Gonza <gonzaloezequielavila@gmail.com>
2023-07-02 15:33:27 -0300

    addedStyles

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from '../Card/Card';
+import styles from "./Cards.module.css"
+
+export default function Cards({characters}) {
+   return (
+   <div className={styles.listItem}>
+      {characters.map(character => 
+         <Card
+         key={character.id}
+         id={character.id}
+         name={character.name}
+         status={character.status}
+         species={character.species}
+         gender={character.gender}
+         origin={character.origin.name}
+         image={character.image}
+         onClose={() => window.alert('Emulamos que se cierra la card')}
+         />)}
+   </div>);
+}
