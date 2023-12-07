b5e84c73af52a691e40cd2e295dee69ff7d3244f
Gonza <gonzaloezequielavila@gmail.com>
2023-12-07 20:55:40 -0300

    M3-03Promises

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,8 +1,20 @@
 const axios = require('axios')
+const URL = `https://rym2.up.railway.app/api/character/`
+const API_KEY = `pi-gonzaloezequielavila`
 
 
 const getCharById = (res,id) => {
-  axios(`https://rym2.up.railway.app/api/character/${id}?key={tuApiKey}`) //siempre que llamemos axios hace automaticamente un llamado tipo .get
+  axios(`${URL}${id}?key=${API_KEY}`) //siempre que llamemos axios hace automaticamente un llamado tipo .get
+  .then(({data}) => {
+    const {id,name,gender,species,origin,image,status} = data;
+    const character = {id,name,gender,species,origin,image,status};
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    return res.end(JSON.stringify(character))
+  })
+  .catch((err)=>{
+    res.writeHead(500, {'Content-Type': 'text/plain'})
+    return res.end(err.message)
+  });
 }
 
 
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,16 +1,29 @@
 const http = require('http'); //Es nativo de Node.js, no hay que instalarlo. Podemos trabajar con servidores
 const data = require('./utils/data')
+const getCharById = require('./controllers/getCharById')
 
 http.createServer((req, res)=>{
   res.setHeader('Access-Control-Allow-Origin', '*'); //Cualquier dominio puede acceder o tener control a lo que devolvamos de nuestro servidor
 
-  if(req.url.includes('/rickandmorty/character')){
-    const id = req.url.split('/').pop() // Vamos a dividir el URL en las / y crear un array y almacenar el ultimo valor en id
-    const character = data.find((char)=>char.id === Number(id)) // Va a iterar en cada character de data hasta que id coincida
+  const {url} = req;
 
-    res.writeHead(200,{'Content-Type': 'application/json'})
-    res.end(JSON.stringify(character)); //Convierte el json en string
-  }
+if(req.url.includes('/rickandmorty/character')){
+  const id = url.split('/').pop()
+  getCharById(res, id);
+}
+
+
+
+
+
+
+  // if(req.url.includes('/rickandmorty/character')){
+  //   const id = req.url.split('/').pop() // Vamos a dividir el URL en las / y crear un array y almacenar el ultimo valor en id
+  //   const character = data.find((char)=>char.id === Number(id)) // Va a iterar en cada character de data hasta que id coincida
+
+  //   res.writeHead(200,{'Content-Type': 'application/json'})
+  //   res.end(JSON.stringify(character)); //Convierte el json en string
+  // }
 
 }).listen(3001, ()=>{
   console.log('Servidor en linea');
